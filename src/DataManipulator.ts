3a25a42d30990fdec16326a3fa73187380328504
bbland1 <104288486+bbland1@users.noreply.github.com>
2023-12-14 21:23:28 -0500

    update the client data mani to have the triggers and data needed

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
+const BOUNDARY_DIFFERENCE = 0.05;
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratioABCtoDEF = priceABC / priceDEF;
+    const upperBound = 1 + BOUNDARY_DIFFERENCE;
+    const lowerBound = 1 - BOUNDARY_DIFFERENCE;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio: ratioABCtoDEF,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratioABCtoDEF > upperBound || ratioABCtoDEF < lowerBound ? ratioABCtoDEF : undefined)
+    };
   }
 }
